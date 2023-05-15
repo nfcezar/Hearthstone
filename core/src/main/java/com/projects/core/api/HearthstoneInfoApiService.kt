cefc94a47d4cd8ec56145afc06ffa2d175415e75
Natalia <nathy.nfs@gmail.com>
2023-05-15 19:32:52 -0300

    - Fixing API address.

--- core/src/main/java/com/projects/core/api/HearthstoneInfoApiService.kt
+++ core/src/main/java/com/projects/core/api/HearthstoneInfoApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object HearthstoneInfoApiService {
-    private const val URL = "https://api.hearthstonejson.com/v1/173781/enUS/cards.json"
+    private const val URL = "https://api.hearthstonejson.com/v1/173781/"
 
     fun createInstance(): HearthstoneApi {
         return Retrofit.Builder()
--- core/src/main/java/com/projects/core/service/HearthstoneApi.kt
+++ core/src/main/java/com/projects/core/service/HearthstoneApi.kt
@@ -4,6 +4,6 @@ import com.projects.core.model.Card
 import retrofit2.http.GET
 
 interface HearthstoneApi {
-    @GET
+    @GET("enUS/cards.json")
     suspend fun getCards(): List<Card>
 }

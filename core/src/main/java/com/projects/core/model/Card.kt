46c50d4307e54c7b615eae6537458eca0d084fd0
Natalia <nathy.nfs@gmail.com>
2023-05-15 19:31:20 -0300

    - Updated card items

--- core/src/main/java/com/projects/core/model/Card.kt
+++ core/src/main/java/com/projects/core/model/Card.kt
@@ -3,11 +3,11 @@ package com.projects.core.model
 import com.google.gson.annotations.SerializedName
 
 data class Card(
-    @SerializedName("id")
+    @SerializedName(value = "id")
     val cardId: String,
-    val name: String,
-    val type: String,
+    val name: String?,
+    val type: String?,
     val race: String?,
     val cardClass: String,
-    val imageUrl: String?
+    val imageUrl: String = "https://art.hearthstonejson.com/v1/render/latest/enUS/512x/"
 )
